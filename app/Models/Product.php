8543cc81321601612a72483688c2792a64f560bc
alamriku <alamriku069@gmail.com>
2023-05-28 20:20:18 +0600

    refactor

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,4 +11,57 @@ class Product extends Model
 
     protected $guarded = [];
 
+    /**
+     * @return void
+     */
+    public function setProduct(): void
+    {
+        $this->id = 1;
+        $this->name = 'Besnik';
+        $this->thumbnail = 'https://ae01.alicdn.com/kf/S95326c72e2b447f580464cbd35249e4dh/2023-New-POEDAGAR-Luxury-Watch-Business-Waterproof-Male-Clock-Luminous-Date-Stainless-Steel-Square-Quartz-Men.jpg_Q90.jpg_.webp';
+    }
+
+    /**
+     * @return void
+     */
+    public function setCategories(): void
+    {
+        $categories = [
+            ['id' => 1, 'name' => 'Premium watch'],
+            ['id' => 2, 'name' => 'Category 2'],
+        ];
+        $categoriesCollect = collect();
+        foreach ($categories as $categoryData) {
+            $category = new Category();
+            $category->id = $categoryData['id'];
+            $category->name = $categoryData['name'];
+
+            $categoriesCollect->push($category);
+        }
+
+        $this->categories = $categoriesCollect;
+    }
+
+    /**
+     * @return void
+     */
+    public function setImages(): void
+    {
+        $images = [
+            ['id'=>1,'url' => 'one of the image/path/1'],
+            ['id'=>2,'url' => 'one of the image/path/2'],
+            ['id'=>3,'url' => 'one of the image/path/3'],
+        ];
+
+        $imageCollection = collect();
+        foreach ($images as $item) {
+            $image = new Gallery();
+            $image->id = $item['id'];
+            $image->url = $item['url'];
+
+            $imageCollection->push($image);
+        }
+
+        $this->images = $imageCollection;
+    }
 }
